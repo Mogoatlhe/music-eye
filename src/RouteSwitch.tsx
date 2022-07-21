29cf539c84d2a15a06899c3a8621166cb3478392
mogoatlhe <iam@ofent.se>
2022-07-21 15:07:25 +0200

    set react router

---
+++ src/RouteSwitch.tsx
@@ -0,0 +1,16 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import NotFound from "./components/NotFound/NotFound";
+import SplashPage from "./components/SplashPage/SplashPage";
+
+const RouteSwitch = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path = "/" element = {<SplashPage />}/>
+        <Route path = "*" element = {<NotFound />}/>
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default RouteSwitch;
