b9baa725c8d80538c0249f75a62e651237598e83
mogoatlhe <iam@ofent.se>
2022-07-22 17:03:19 +0200

    change colour

--- src/components/SplashPage/Header.tsx
+++ src/components/SplashPage/Header.tsx
@@ -2,9 +2,9 @@ import logo from "../../assets/musiceye-logo.png";
 
 const SplashPageHeader = () => {
   return (
-    <div className = "flex p-4 justify-between items-center border-b border-neutral-800 h-1/16">
+    <div className = "flex p-4 justify-between items-center border-b border-neutral-700 h-1/16">
       <img src = {logo} alt = "musiceye logo" className = "w-14 h-8"/>
-      <div className = "flex p-4 pr-2 justify-between w-60 pl-20 items-center font-stoke text-xl">
+      <div className = "flex p-4 pr-2 justify-between w-60 pl-20 items-center font-stoke text-xl text-neutral-700">
         <p>Sign in</p>
         <p>Sign up</p>
       </div>
