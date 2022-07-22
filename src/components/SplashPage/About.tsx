08d8c9a3ef2ed14775fad0bce0802497023de6ec
mogoatlhe <iam@ofent.se>
2022-07-22 17:03:39 +0200

    add splash page about component

---
+++ src/components/SplashPage/About.tsx
@@ -0,0 +1,9 @@
+
+const About = () => {
+  return (
+    <div className = "h-[88.5%] bg-neutral-700">
+    </div>
+  );
+}
+
+export default About;
--- src/components/SplashPage/SplashPage.tsx
+++ src/components/SplashPage/SplashPage.tsx
@@ -1,9 +1,11 @@
+import About from "./About";
 import Header from "./Header";
 
 const SplashPage = () => {
   return (
     <div className = "h-screen">
       <Header />
+      <About />
     </div>
   )
 }
