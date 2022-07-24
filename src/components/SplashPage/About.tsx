0d611d84e113291b93e34ec3aa930ea5f209ed05
mogoatlhe <iam@ofent.se>
2022-07-24 16:30:09 +0200

    add the second splash image

--- src/components/SplashPage/About.tsx
+++ src/components/SplashPage/About.tsx
@@ -1,9 +1,11 @@
 import image1 from "../../assets/splash-images/scr-shot-1.png";
+import image2 from "../../assets/splash-images/scr-shot-2.png";
 
 const About = () => {
   return (
     <div className = "relative h-[100%] pt-24 bg-neutral-700 max-width overflow-hidden">
-      <img src = {image1} alt = "song playing screenshot" className = "absolute ml-36 opacity-50"/>
+      <img src = {image1} alt = "song playing screenshot" className = "absolute ml-36 opacity-40"/>
+      <img src = {image2} alt = "song playing screenshot" className = "absolute mt-80 -ml-32 opacity-40"/>
     </div>
   );
 }
