44a786d5289347a76c1d5548324e459703098c86
mogoatlhe <iam@ofent.se>
2022-07-24 16:18:30 +0200

    make splash page header fixed

--- src/components/SplashPage/About.tsx
+++ src/components/SplashPage/About.tsx
@@ -2,7 +2,7 @@ import image1 from "../../assets/splash-images/scr-shot-1.png";
 
 const About = () => {
   return (
-    <div className = "relative h-[88.5%] bg-neutral-700 max-width overflow-hidden">
+    <div className = "relative h-[100%] pt-24 bg-neutral-700 max-width overflow-hidden">
       <img src = {image1} alt = "song playing screenshot" className = "absolute ml-36 opacity-50"/>
     </div>
   );
