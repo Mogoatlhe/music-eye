d0de59beff3f20ba42a30a3eb2c5c7e19f515d84
mogoatlhe <iam@ofent.se>
2022-07-24 16:03:44 +0200

    add the first splash page image

--- src/components/SplashPage/About.tsx
+++ src/components/SplashPage/About.tsx
@@ -1,7 +1,9 @@
+import image1 from "../../assets/splash-images/scr-shot-1.png";
 
 const About = () => {
   return (
-    <div className = "h-[88.5%] bg-neutral-700">
+    <div className = "relative h-[88.5%] bg-neutral-700 max-width overflow-hidden">
+      <img src = {image1} alt = "song playing screenshot" className = "absolute ml-36 opacity-50"/>
     </div>
   );
 }
