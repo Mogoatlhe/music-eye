0ceca111505f1637b0d680bf41e93fb2f7d638a4
mogoatlhe <iam@ofent.se>
2022-07-24 17:56:06 +0200

    add "Special Elite" font to project

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,9 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
     fontFamily: {
-      stoke: ['"Stoke"', "serif"],
+      stoke: ['"Stoke"', 'serif'],
+      specialElite: ['"Special Elite"', 'cursive'],
     },
     extend: {},
   },
