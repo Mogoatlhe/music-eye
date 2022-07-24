7f14883c528a09c44911409933125be9638ec0ba
mogoatlhe <iam@ofent.se>
2022-07-24 17:07:56 +0200

    add fourth image

--- src/components/SplashPage/About.tsx
+++ src/components/SplashPage/About.tsx
@@ -1,27 +1,33 @@
-import image1 from '../../assets/splash-images/scr-shot-1.png'
-import image2 from '../../assets/splash-images/scr-shot-2.png'
-import image3 from '../../assets/splash-images/scr-shot-3.png'
+import image1 from '../../assets/splash-images/scr-shot-1.png';
+import image2 from '../../assets/splash-images/scr-shot-2.png';
+import image3 from '../../assets/splash-images/scr-shot-3.png';
+import image4 from '../../assets/splash-images/scr-shot-4.jpg';
 
 const About = () => {
-    return (
-        <div className="relative h-[100%] pt-24 bg-neutral-700 overflow-hidden">
-            <img
-                src={image1}
-                alt="song playing screenshot"
-                className="absolute ml-36 opacity-40"
-            />
-            <img
-                src={image2}
-                alt="song playing screenshot"
-                className="absolute mt-80 -ml-32 opacity-40"
-            />
-            <img
-                src={image3}
-                alt="song playing screenshot"
-                className="absolute opacity-40"
-            />
-        </div>
-    )
-}
+  return (
+    <div className="relative h-[100%] pt-24 bg-neutral-700 overflow-hidden">
+      <img
+        src={image1}
+        alt="song playing screenshot"
+        className="absolute ml-36 opacity-40"
+      />
+      <img
+        src={image2}
+        alt="song playing screenshot"
+        className="absolute mt-80 -ml-32 opacity-40"
+      />
+      <img
+        src={image3}
+        alt="song playing screenshot"
+        className="absolute opacity-40 w-8/12 mt-16 ml-8"
+      />
+      <img
+        src={image4}
+        alt="song playing screenshot"
+        className="absolute opacity-40 w-8/12 mt-72 ml-28"
+      />
+    </div>
+  );
+};
 
-export default About
+export default About;
