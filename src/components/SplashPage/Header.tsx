afd733363bc6987514cc3660e7201aa3f9648a56
mogoatlhe <iam@ofent.se>
2022-08-07 12:19:32 +0200

    fix spelling error

--- src/components/SplashPage/Header.tsx
+++ src/components/SplashPage/Header.tsx
@@ -12,10 +12,10 @@ const SplashPageHeader = () => {
     <div className="flex fixed z-10 bg-white w-screen p-4 justify-between items-center border-b border-neutral-700 h-1/16">
       <img src={logo} alt="musiceye logo" className="w-14 h-8" />
       <div className="flex p-4 pr-2 justify-between w-60 pl-20 items-center font-stoke text-xl text-neutral-700">
-        <button onClick={() => dispatch(changeAccessType('Sign in'))}>
+        <button onClick={() => dispatch(changeAccessType('Sign In'))}>
           Sign in
         </button>
-        <button onClick={() => dispatch(changeAccessType('Sign up'))}>
+        <button onClick={() => dispatch(changeAccessType('Sign Up'))}>
           Sign up
         </button>
       </div>
