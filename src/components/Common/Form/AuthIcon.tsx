abac98a6c0dcbd0b0bd418ea2d29543746f9949c
mogoatlhe <iam@ofent.se>
2022-08-11 12:47:56 +0200

    change button type attribute

--- src/components/Common/Form/AuthIcon.tsx
+++ src/components/Common/Form/AuthIcon.tsx
@@ -3,12 +3,7 @@ import { IconType } from 'react-icons';
 
 const AuthIcon = ({ icon }: { icon: IconType }) => {
   return (
-    <button
-      className="border border-jet rounded-md p-4 text-4xl"
-      onClick={(e) => {
-        e.preventDefault();
-      }}
-    >
+    <button className="border border-jet rounded-md p-4 text-4xl" type="button">
       {React.createElement(icon)}
     </button>
   );
